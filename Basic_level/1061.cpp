47a15d1599a53215a82b198ccaa8f98c0e566048
ZHEN603 <a513515732@hotmail.com>
2023-07-08 21:21:01 +0800

    ac1061

--- Basic_level/1061.cpp
+++ Basic_level/1061.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 int main()
 {
-
+    int n,m,t;
+    cin>>n>>m;
+    vector<int> marks(m),corrects(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin>>marks[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin>>corrects[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int sum=0;
+        for (int j = 0; j < m; j++)
+        {
+            cin>>t;
+            if (t==corrects[j])
+            {
+                sum+=marks[j];
+            }
+        }
+        cout<<sum<<endl;
+    }
 }
