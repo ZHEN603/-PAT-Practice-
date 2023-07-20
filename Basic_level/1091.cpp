6f82451f3e91dbe7febc6afd470c4b69824a0ea6
ZHEN603 <a513515732@hotmail.com>
2023-07-20 15:34:40 +0800

    ac1091

--- Basic_level/1091.cpp
+++ Basic_level/1091.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
-#include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
 #include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    int n,m,k;
+    cin>>m;
+    for (int i = 0; i < m; i++)
+    {
+        cin>>k;
+        int j = 1;
+        for (; j < 10; j++)
+        {
+            if (to_string(j*k*k).substr(to_string(j*k*k).size()-to_string(k).size(),to_string(k).size())==to_string(k))
+            {
+                cout<<j<<" "<<j*k*k<<endl;
+                break;
+            }
+        }
+        if (j==10)
+        {
+            cout<<"No"<<endl;
+        }
+    }
 }
