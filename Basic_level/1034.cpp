dd7dd9697bab0ce16a7bb3a241f90e552d439af9
ZHEN603 <a513515732@hotmail.com>
2023-08-12 20:54:29 +0800

    updata1034 8.12

--- Basic_level/1034.cpp
+++ Basic_level/1034.cpp
@@ -7,7 +7,50 @@
 #include<vector>
 #include<set>
 using namespace std;
+string show(int numerator,int denominator)
+{
+    string res="";
+    for (int i = abs(numerator); i >= 2; i--)
+    {
+        if (numerator%i==0&&denominator%i==0)
+        {
+            numerator/=i;
+            denominator/=i;
+            break;
+        }
+    }
+    if (numerator*denominator<0)
+    {
+        res+="-";
+    }
+    numerator=abs(numerator);
+    denominator=abs(denominator);
+    if (numerator>=denominator)
+    {
+        res+=to_string(numerator/denominator);
+        if (numerator%denominator)
+        {
+            res+=" ";
+            res+=to_string(numerator%denominator);
+            res+="/";
+            res+=to_string(denominator);
+        }
+    }
+    else
+    {
+        res+=to_string(numerator)+"/"+to_string(denominator);
+    }
+    if (res[0]=='-')
+    {
+        res+=")";
+        res="("+res;
+    }
+    return res;
+}
 int main()
 {
-    
+    string a,b,c;
+    cin>>a>>b;
+    int numeratorA=stoi(a.substr(0,a.find('/')+1)),denominatorA=stoi(a.substr(a.find('/')+1,a.size()-a.find('/'))),numeratorB=stoi(b.substr(0,b.find('/')+1)),denominatorB=stoi(b.substr(b.find('/')+1,b.size()-b.find('/')));
+    cout<<show(numeratorA,denominatorA)<<" + "<<show(numeratorB,denominatorB)<<" = "<<show(numeratorA*denominatorB+numeratorB*denominatorA,denominatorA*denominatorB);
 }
