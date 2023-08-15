546934d3f7905e06737397d38fecd023148ca556
ZHEN603 <a513515732@hotmail.com>
2023-08-15 10:49:49 +0800

    ac1036

--- Basic_level/1036.cpp
+++ Basic_level/1036.cpp
@@ -1,13 +1,27 @@
 #include<iostream>
-#include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    int n;
+    char c;
+    cin>>n>>c;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<c;
+    }
+    cout<<endl;
+    for (int i = 0; i < int(n/2.0+0.5)-2; i++)
+    {
+        cout<<c;
+        for (int j = 0; j < n-2; j++)
+        {
+            cout<<" ";
+        }
+        cout<<c<<endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<c;
+    }
+    cout<<endl;
 }
