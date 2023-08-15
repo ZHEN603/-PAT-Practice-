636d793dc28819cda35ee790ec976e4a81961402
ZHEN603 <a513515732@hotmail.com>
2023-08-15 11:01:13 +0800

    ac1037

--- Basic_level/1037.cpp
+++ Basic_level/1037.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
-#include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    int Pg,Ps,Pk,Ag,As,Ak,P,A,res;
+    scanf("%d.%d.%d %d.%d.%d",&Pg,&Ps,&Pk,&Ag,&As,&Ak);
+    P=Pg*17*29+Ps*29+Pk;
+    A=Ag*17*29+As*29+Ak;
+    res=A-P;
+    cout<<res/(17*29)<<"."<<abs(res%(17*29)/29)<<"."<<abs(res%29);
 }
