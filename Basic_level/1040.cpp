6d896d11a155b192c6a095dbbf3fa965833d5d39
ZHEN603 <a513515732@hotmail.com>
2023-08-16 10:39:37 +0800

    ac1040

---
+++ Basic_level/1040.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<map>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<set>
+using namespace std;
+int main()
+{
+    string t;
+    cin>>t;
+    
+}
