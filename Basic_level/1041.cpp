c911cfaf05f50c9fb9023ff18de4271f5f2d5771
ZHEN603 <a513515732@hotmail.com>
2023-08-20 17:48:54 +0800

    ac1041

--- Basic_level/1041.cpp
+++ Basic_level/1041.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 #include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    map<int,pair<string,int>> data;
+    int n,t,pos;
+    string s;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>s>>t>>pos;
+        data[t]=make_pair(s,pos);
+    }
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>t;
+        cout<<data[t].first<<" "<<data[t].second<<endl;
+    }
 }
