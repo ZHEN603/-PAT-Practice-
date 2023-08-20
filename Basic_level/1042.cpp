405420b2fb0581505e899f1b629fbda661e109f8
ZHEN603 <a513515732@hotmail.com>
2023-08-20 18:04:41 +0800

    ac1042

--- Basic_level/1042.cpp
+++ Basic_level/1042.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 #include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
 #include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    map<char,int> counter;
+    string s;
+    getline(cin,s);
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (tolower(s[i])>='a'&&tolower(s[i])<='z')
+        {
+            counter[tolower(s[i])]++;
+        }
+    }
+    auto max=counter.begin();
+    for (auto i = counter.begin(); i != counter.end(); i++)
+    {
+        if (i->second>max->second)
+        {
+            max=i;
+        }
+    }
+    cout<<max->first<<" "<<max->second;
 }
