39b15cca5fbd064ecfb0acc13ed2622e80c14492
ZHEN603 <a513515732@hotmail.com>
2023-08-21 11:14:05 +0800

    ac1043

--- Basic_level/1043.cpp
+++ Basic_level/1043.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 #include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
 #include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    string sample="PATest",t;
+    cin>>t;
+    map<char,int> counter;
+    for (int i = 0; i < t.size(); i++)
+    {
+        counter[t[i]]++;
+    }
+    while (sample.size())
+    {
+        for (auto i = sample.begin(); i != sample.end(); i++)
+        {
+            if (counter[*i]>0)
+            {
+                cout<<(*i);
+                counter[*i]--;
+            }
+            else if (counter[*i]==0)
+            {
+                sample.erase(i);
+                i--;
+            }
+        }
+    }
 }
