38f914a9189d195e4cf8402392044cf7123c5ea0
ZHEN603 <a513515732@hotmail.com>
2023-08-24 14:39:31 +0800

    ac1046

---
+++ Basic_level/1046.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<map>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<set>
+using namespace std;
+int main()
+{
+    
+}
