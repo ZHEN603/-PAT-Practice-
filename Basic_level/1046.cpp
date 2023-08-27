08b68b7fa7e921f828adaaed5a1f283b940db09f
ZHEN603 <a513515732@hotmail.com>
2023-08-27 15:45:22 +0800

    ac1046

--- Basic_level/1046.cpp
+++ Basic_level/1046.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
-#include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    int n,j,y,jia,yi,counter[2]={0};
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>j>>jia>>y>>yi;
+        if (j+y==jia)
+        {
+            if (j+y!=yi)
+            {
+                counter[1]++;
+            }
+        }
+        else if (j+y==yi)
+        {
+            counter[0]++;
+        }
+    }
+    cout<<counter[0]<<" "<<counter[1];
 }
