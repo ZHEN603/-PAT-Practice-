eb470f11b6cc592bdf0241c44f50c697a5aba83c
ZHEN603 <a513515732@hotmail.com>
2023-08-27 15:36:44 +0800

    ac1047

--- Basic_level/1047.cpp
+++ Basic_level/1047.cpp
@@ -1,13 +1,22 @@
 #include<iostream>
 #include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
-#include<set>
 using namespace std;
 int main()
 {
-    
+    map<string,int> data;
+    int n,m,max=0;
+    string t,res;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>t>>m;
+        t=t.substr(0,t.find("-"));
+        data[t]+=m;
+        if (data[t]>max)
+        {
+            max=data[t];
+            res=t;
+        }
+    }
+    cout<<res<<" "<<max;
 }
