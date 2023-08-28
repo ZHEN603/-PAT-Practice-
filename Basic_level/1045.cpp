490335ac58e3f02848f74770d90898d6685c18da
ZHEN603 <a513515732@hotmail.com>
2023-08-28 18:56:27 +0800

    ac1045

--- Basic_level/1045.cpp
+++ Basic_level/1045.cpp
@@ -1,13 +1,35 @@
 #include<iostream>
-#include<map>
-#include<cmath>
-#include<algorithm>
-#include<iomanip>
-#include<string>
-#include<vector>
 #include<set>
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin>>n;
+    int max[n],min[n],data[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>data[i];
+    }
+    max[0]=data[0];
+    min[n-1]=data[n-1];
+    for (int i = 1; i < n; i++)
+    {
+        max[i]=max[i-1]<data[i]?data[i]:max[i-1];
+        min[n-i-1]=min[n-i]>data[n-i-1]?data[n-i-1]:min[n-i];
+    }
+    set<int> res;
+    for (int i = 0; i < n; i++)
+    {
+        if (max[i]==min[i])
+        {
+            res.insert(data[i]);
+        }
+    }
+    cout<<res.size()<<endl;
+    for (auto i = res.begin(); i != res.end(); i++)
+    {
+        cout<<(i==res.begin()?"":" ");
+        cout<<*i;
+    }
+    cout<<endl;
 }
